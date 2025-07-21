f2e1a1627f041caa712a589fc5877a79cf9c46c7
Kohei Oshio (Gadgetton) <k-oshio@gadgetton.com>
2025-07-21 23:49:00 +0900

    :recycle: ボタンクリックイベントの命名変更

--- public/ModalWindow.js
+++ public/ModalWindow.js
@@ -3,14 +3,14 @@ const modal = document.getElementsByClassName("modal")[0];
 
 // 実行ボタンクリックイベント
 const buttonRun = document.getElementById("run-button");
-buttonRun.addEventListener("click", clickBlockButton);
-function clickBlockButton() {
+buttonRun.addEventListener("click", clickRunButton);
+function clickRunButton() {
   modal.style.display = "block";;
 }
 
 // モーダルウィンドウの閉じるボタンクリックイベント
 const buttonClose = document.getElementsByClassName("modal-close")[0];
-buttonClose.addEventListener("click", modalClose);
-function modalClose() {
+buttonClose.addEventListener("click", clickCloseButton);
+function clickCloseButton() {
   modal.style.display = "none";
 }
