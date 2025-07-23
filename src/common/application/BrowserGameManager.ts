d2b202430c47debb2abebb60033fd4727cc26ef4
Kohei Oshio (Gadgetton) <k-oshio@gadgetton.com>
2025-07-24 02:20:19 +0900

    :sparkles: BrowserGameManagerのスケルトンを実装

--- src/common/application/BrowserGameManager.ts
+++ src/common/application/BrowserGameManager.ts
@@ -1,25 +1,60 @@
+import { IFrameUpdater } from "./IFrameUpdater";
+import { IBrowserGame } from "./IBrowserGame";
+
 /**
  * ブラウザゲーム管理クラス(エントリポイント)
  */
 export class BrowserGameManager {
     /**
+     * ゲーム停止フラグ
+     */
+    private stopFlag: boolean = null!;
+
+    /**
+     * フレーム更新器
+     */
+    private frameUpdater: IFrameUpdater = null!;
+
+    /**
+     * ブラウザゲームオブジェクト
+     */
+    private browserGame: IBrowserGame = null!;
+
+    /**
      * コンストラクタ
      */
     public constructor() {
-        console.log("BrowserGameManager.constructor()");
+        throw new Error("未実装");
+    }
+
+    /**
+     * Canvas2Dコンテキストの取得
+     * @param canvas Canvas要素
+     * @returns Canvas2Dコンテキスト
+     */
+    private getContext(canvas: HTMLElement): CanvasRenderingContext2D {
+        throw new Error("未実装");
     }
 
     /**
      * ゲームの開始
      */
     public start(): void {
-        console.log("BrowserGameManager.start()");
+        throw new Error("未実装");
+    }
+
+    /**
+     * ゲームの更新
+     * @param deltaTime 前回の更新からの経過時間
+     */
+    private update(deltaTime: number): void {
+        throw new Error("未実装");
     }
 
     /**
      * ゲームの停止
      */
     public stop(): void {
-        console.log("BrowserGameManager.stop()");
+        throw new Error("未実装");
     }
 }
