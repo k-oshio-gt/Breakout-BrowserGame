1725fb6b4b027e64d95db47ac7bb228fa599e217
Kohei Oshio (Gadgetton) <k-oshio@gadgetton.com>
2025-07-26 17:06:26 +0900

    :sparkles: Timerのロジックを実装

--- src/common/application/FrameTimer.ts
+++ src/common/application/FrameTimer.ts
@@ -11,14 +11,21 @@ export class FrameTimer {
      * 計測開始
      */
     public start(): void {
-        throw new Error("未実装");
+        this.startTime = performance.now() / 1000;
     }
 
     /**
      * 計測
+     * (計測誤差は5[us]程度)
      * @return 経過時間[s]
+     * @throws {ReferenceError} start()が呼ばれていない場合にスローする例外
      */
     public measure(): number {
-        throw new Error("未実装");
+        if (this.startTime === undefined) {
+            // TODO: エラーログが必要
+            throw new ReferenceError("measure()の前にstart()が呼ばれていません");
+        }
+        let nowTime = performance.now() / 1000;
+        return nowTime - this.startTime;
     }
 }
